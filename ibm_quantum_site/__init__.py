770a1880b2a713dc38e291f66adcc6d594d20e9c
Andy Gallo <agallojr@518computing.com>
2025-05-25 20:58:29 -0400

    reorg for production deployments

--- ibm_quantum_site/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-IBM Quantum Site
-"""
-from ibm_quantum_site.IBMQuantumSite import IBMQuantumSite
-
-__all__ = ["IBMQuantumSite"]
---
+++ src/ibm_quantum_site/__init__.py
@@ -0,0 +1,7 @@
+"""
+IBM Quantum Site
+"""
+from ibm_quantum_site.IBMQuantumSite import IBMQuantumSite
+from ibm_quantum_site.IBMQuantumVenvSite import IBMQuantumVenvSite
+
+__all__ = ["IBMQuantumSite", "IBMQuantumVenvSite"]
